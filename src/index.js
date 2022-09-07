82fea927e3bd09cb3706887091bd18caa67753e3
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-09-08 01:02:37 +0300

    display list of items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // import './style.css';
+import display from './modules/display.js';
+// import showPopup from './modules/showPopup.js';
+// import likes from './modules/likes.js';
+// import reserveAwaiting from './modules/RESERVATION.js';
+// import icon from '../media/tvicon.png';
+
+// const logo = document.querySelector('.logo img');
+// logo.src = icon;
+
+display();
+// setTimeout(() => {
+//   likes.displayLikes();
+//   likes.avtivateLikeBtns();
+//   showPopup();
+//   reserveAwaiting();
+// }, 1000);
