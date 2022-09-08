82a18eb8cfce497b1b80cf658fd68252ce650dd1
ademola <ademolaowoeye07@gmail.com>
2022-09-08 22:27:43 +0100

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import './style.css';
 import display from './modules/display.js';
 import likes from './modules/likes.js';
 
+import commentPopups from './modules/COMMENT.js';
+
 display();
 setTimeout(() => {
   likes.displayLikes();
   likes.avtivateLikeBtns();
 }, 1000);
-
-import commentPopups from './modules/COMMENT.js';
 commentPopups();
-
